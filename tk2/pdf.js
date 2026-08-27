fec905395752bcd585a6feee2a03d7d2649e1d0e
modebrecht <137332207+modebrecht@users.noreply.github.com>
2026-08-27 23:31:25 +0200

    Load A1-A2 attempt tracking for PDF consistency

--- tk2/pdf.js
+++ tk2/pdf.js
@@ -1,6 +1,6 @@
 (function(){
   'use strict';
-  document.write('<script src="courseAccess.js"><\/script><script src="pdf-base.js"><\/script>');
+  document.write('<script src="courseAccess.js"><\/script><script src="pdf-base.js"><\/script><script src="quest-attempts.js"><\/script>');
   if (/\/tk2\/A7\.html$/i.test(location.pathname)) {
     document.write('<script src="a7-evidence-ui.js"><\/script>');
   }
